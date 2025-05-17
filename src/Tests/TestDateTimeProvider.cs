5bda4b16edb01ba9e767678acb6ea94b7aa9edf6
Tsepenshchikov Vladimir <tsepenshchikov@mindbox.cloud>
2025-05-17 22:55:58 +0500

    feat: test date time provider

---
+++ src/Tests/TestDateTimeProvider.cs
@@ -0,0 +1,19 @@
+﻿using BabloBudget.Api;
+
+namespace Tests;
+
+internal sealed class TestDateTimeProvider : IDateTimeProvider
+{
+    public DateTime UtcNow => _dateTimeProvider();
+    public DateOnly UtcNowDateOnly => DateOnly.FromDateTime(UtcNow);
+    
+    private readonly Func<DateTime> _dateTimeProvider;
+
+    private TestDateTimeProvider(Func<DateTime> dateTimeProvider)
+    {
+        _dateTimeProvider = dateTimeProvider;
+    }
+    
+    internal static IDateTimeProvider Create(DateTime dateTimeToProvide) =>
+        new TestDateTimeProvider(() => dateTimeToProvide);
+}

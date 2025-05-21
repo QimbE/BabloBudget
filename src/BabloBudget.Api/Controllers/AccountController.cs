8025dcd727a07a897fc7fef3107ae53129a0c1c1
Владимир Цепенщиков <vovar3390@gmail.com>
2025-05-21 20:28:35 +0500

    refactor: use extension method

--- src/BabloBudget.Api/Controllers/AccountController.cs
+++ src/BabloBudget.Api/Controllers/AccountController.cs
@@ -18,30 +18,25 @@ public class AccountController(IDbContextFactory<ApplicationDbContext> dbContext
         [FromQuery]decimal basisSum,
         CancellationToken token)
     {
-        var claims = HttpContext.User.Claims;
-        var userId = claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier);
-        if (userId == null)
-        {
+        var userId = HttpContext.User.TryParseUserId();
+        
+        if (userId is null)
             return BadRequest("Unable to identify user");
-        }
-        if (!Guid.TryParse(userId.Value, out var userIdGuid) || userIdGuid == Guid.Empty)
-        {
-            return BadRequest("Unable to identify user");
-        }
+        
         var result = await dbContextFactory.ExecuteAndCommitAsync<IActionResult>(async dbContext =>
         {
-            var isUserExists = await dbContext.Users.AnyAsync(u => u.Id == userIdGuid, token);
+            var isUserExists = await dbContext.Users.AnyAsync(u => u.Id == userId, token);
             if (!isUserExists)
             {
                 return NotFound("User not found");
             }
-            var isAccountExists = await dbContext.Accounts.AnyAsync(a => a.Id == userIdGuid, token);
+            var isAccountExists = await dbContext.Accounts.AnyAsync(a => a.Id == userId, token);
             if (isAccountExists)
             {
                 return BadRequest("Account already exists");
             }
             var money = Money.Create(basisSum);
-            var account = Account.Create(money, userIdGuid);
+            var account = Account.Create(money, userId.Value);
             var accountDto = AccountDto.FromDomainModel(account);
             dbContext.Accounts.Add(accountDto);
             await dbContext.SaveChangesAsync(token);
@@ -55,24 +50,19 @@ public class AccountController(IDbContextFactory<ApplicationDbContext> dbContext
     [HttpGet("GetBasisSum")]
     public async Task<IActionResult> GetBasisSumAsync(CancellationToken token)
     {
-        var claims = HttpContext.User.Claims;
-        var userId = claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier);
-        if (userId == null)
-        {
+        var userId = HttpContext.User.TryParseUserId();
+        
+        if (userId is null)
             return BadRequest("Unable to identify user");
-        }
-        if (!Guid.TryParse(userId.Value, out var userIdGuid) || userIdGuid == Guid.Empty)
-        {
-            return BadRequest("Unable to identify user");
-        }
+
         var result = await dbContextFactory.ExecuteAndCommitAsync<IActionResult>(async dbContext =>
         {
-            var isUserExists = await dbContext.Users.AnyAsync(u => u.Id == userIdGuid, token);
+            var isUserExists = await dbContext.Users.AnyAsync(u => u.Id == userId, token);
             if (!isUserExists)
             {
                 return NotFound("User not found");
             }
-            var userAccount = await dbContext.Accounts.SingleOrDefaultAsync(a => a.Id == userIdGuid, token);
+            var userAccount = await dbContext.Accounts.SingleOrDefaultAsync(a => a.Id == userId, token);
             if (userAccount is null)
             {
                 return NotFound("Account not found");
@@ -89,24 +79,19 @@ public class AccountController(IDbContextFactory<ApplicationDbContext> dbContext
         [FromQuery] decimal newBasisSum,
         CancellationToken token)
     {
-        var claims = HttpContext.User.Claims;
-        var userId = claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier);
-        if (userId == null)
-        {
+        var userId = HttpContext.User.TryParseUserId();
+        
+        if (userId is null)
             return BadRequest("Unable to identify user");
-        }
-        if (!Guid.TryParse(userId.Value, out var userIdGuid) || userIdGuid == Guid.Empty)
-        {
-            return BadRequest("Unable to identify user");
-        }
+
         var result = await dbContextFactory.ExecuteAndCommitAsync<IActionResult>(async dbContext =>
         {
-            var isUserExists = await dbContext.Users.AnyAsync(u => u.Id == userIdGuid, token);
+            var isUserExists = await dbContext.Users.AnyAsync(u => u.Id == userId, token);
             if (!isUserExists)
             {
                 return NotFound("User not found");
             }
-            var userAccount = await dbContext.Accounts.SingleOrDefaultAsync(a => a.Id == userIdGuid, token);
+            var userAccount = await dbContext.Accounts.SingleOrDefaultAsync(a => a.Id == userId, token);
             if (userAccount is null)
             {
                 return NotFound("Account not found");

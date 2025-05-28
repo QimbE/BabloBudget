ba54911914a1e938791744f173732f7ea777d5f1
Stephen <sokhrannykhs@gmail.com>
2025-05-29 02:58:08 +0500

    feat: add get controllers for account entry

--- src/BabloBudget.Api/Controllers/AccountEntryController.cs
+++ src/BabloBudget.Api/Controllers/AccountEntryController.cs
@@ -5,6 +5,7 @@ using BabloBudget.Api.Repository.Resilience;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Collections.Immutable;
 using System.Text.Json.Serialization;
 
 namespace BabloBudget.Api.Controllers;
@@ -87,6 +88,158 @@ public class AccountEntryController(
             await dbContext.SaveChangesAsync(token);
 
             return Ok();
+        },
+        cancellationToken: token);
+
+        return result;
+    }
+
+    [HttpGet("GetById")]
+    public async Task<IActionResult> GetAccountEntryAsync(
+        [FromQuery] Guid accountEntryId,
+        CancellationToken token)
+    {
+        var userId = HttpContext.User.TryParseUserId();
+
+        if (userId is null)
+            return BadRequest("Unable to identify user");
+
+        var result = await dbContextFactory.ExecuteReadonlyAsync<IActionResult>(async dbContext =>
+        {
+            var accountEntryDto = await dbContext.AccountEntries
+                .SingleOrDefaultAsync(a => a.Id == accountEntryId, token);
+
+            if (accountEntryDto is null)
+                return NotFound("Account entry does not exist");
+
+            if (accountEntryDto.AccountId != userId)
+                return Forbid();
+
+            return Ok(accountEntryDto);
+        },
+        cancellationToken: token);
+
+        return result;
+    }
+
+    [HttpGet("GetAll")]
+    public async Task<IActionResult> GetAllAccountEntriesAsync(CancellationToken token)
+    {
+        var userId = HttpContext.User.TryParseUserId();
+
+        if (userId is null)
+            return BadRequest("Unable to identify user");
+
+        var result = await dbContextFactory.ExecuteReadonlyAsync<IActionResult>(async dbContext =>
+        {
+            var accountEntries = (await dbContext.AccountEntries
+                .Where(ae => ae.AccountId == userId)
+                .ToListAsync(token))
+                .ToImmutableList();
+
+            return Ok(accountEntries);
+        },
+        cancellationToken: token);
+
+        return result;
+    }
+
+    [HttpGet("GetInDateSpan")]
+    public async Task<IActionResult> GetAccountEntriesInDateSpanAsync(
+        [FromQuery] DateOnly startDate,
+        [FromQuery] DateOnly endDate,
+        CancellationToken token)
+    {
+        var userId = HttpContext.User.TryParseUserId();
+
+        if (userId is null)
+            return BadRequest("Unable to identify user");
+
+        if (startDate > endDate)
+            return BadRequest("Invalid date span");
+
+        var result = await dbContextFactory.ExecuteReadonlyAsync<IActionResult>(async dbContext =>
+        {
+            var accountEntries = (await dbContext.AccountEntries
+                .Where(ae => 
+                    ae.AccountId == userId && 
+                    startDate <= ae.DateUtc && ae.DateUtc <= endDate)
+                .ToListAsync(token))
+                .ToImmutableList();
+
+            return Ok(accountEntries);
+        },
+        cancellationToken: token);
+
+        return result;
+    }
+
+    [HttpGet("GetAllByCategory")]
+    public async Task<IActionResult> GetAllAccountEntriesByCategoryAsync(
+        [FromQuery] Guid? categoryId, 
+        CancellationToken token)
+    {
+        var userId = HttpContext.User.TryParseUserId();
+
+        if (userId is null)
+            return BadRequest("Unable to identify user");
+
+        var result = await dbContextFactory.ExecuteReadonlyAsync<IActionResult>(async dbContext =>
+        {
+            var categoryDto = categoryId is null
+                ? null
+                : await dbContext.Categories.SingleOrDefaultAsync(c => c.Id == categoryId, token);
+
+            if (categoryDto is null != categoryId is null)
+                return BadRequest($"Category with id {categoryId} does not exist");
+
+            var accountEntries = (await dbContext.AccountEntries
+                .Where(ae => 
+                    ae.AccountId == userId &&
+                    ae.CategoryId == categoryId)
+                .ToListAsync(token))
+                .ToImmutableList();
+
+            return Ok(accountEntries);
+        },
+        cancellationToken: token);
+
+        return result;
+    }
+
+    [HttpGet("GetByCategoryInDateSpan")]
+    public async Task<IActionResult> GetAccountEntriesByCategoriesInDateSpanAsync(
+        [FromQuery] DateOnly startDate,
+        [FromQuery] DateOnly endDate,
+        [FromQuery] Guid? categoryId,
+        CancellationToken token)
+    {
+        var userId = HttpContext.User.TryParseUserId();
+
+        if (userId is null)
+            return BadRequest("Unable to identify user");
+
+        if (startDate > endDate)
+            return BadRequest("Invalid date span");
+
+        var result = await dbContextFactory.ExecuteReadonlyAsync<IActionResult>(async dbContext =>
+        {
+            var categoryDto = categoryId is null
+                ? null
+                : await dbContext.Categories.SingleOrDefaultAsync(c => c.Id == categoryId, token);
+
+            if (categoryDto is null != categoryId is null)
+                return BadRequest($"Category with id {categoryId} does not exist");
+
+            var accountEntries = (await dbContext.AccountEntries
+                .Where(ae =>
+                    ae.AccountId == userId &&
+                    startDate <= ae.DateUtc && ae.DateUtc <= endDate &&
+                    ae.CategoryId == categoryId)
+                .ToListAsync(token))
+                .ToImmutableList();
+
+            return Ok(accountEntries);
         },
         cancellationToken: token);
 
